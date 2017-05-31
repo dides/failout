2ba89a729daa9359283841c1bebdde33ac30fb3c
dides <shornikovdanila@mail.ru>
2017-05-31 19:21:23 +0300

    vf

---
+++ Gropsserc.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Gropsserc
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(419, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -8);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(126, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(126, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(554, 75);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 20);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "поиск груп";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(12, 122);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(627, 224);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(843, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // listView2
+            // 
+            this.listView2.LargeImageList = this.imageList2;
+            this.listView2.Location = new System.Drawing.Point(796, 92);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(147, 247);
+            this.listView2.TabIndex = 10;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(802, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(755, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // listView3
+            // 
+            this.listView3.Location = new System.Drawing.Point(648, 99);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(142, 247);
+            this.listView3.TabIndex = 13;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Все групы";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(793, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Участники груп";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(645, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Выбранные групы";
+            // 
+            // Gropsserc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 371);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Gropsserc";
+            this.Text = "Gropsserc";
+            this.Load += new System.EventHandler(this.Gropsserc_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ Gropsserc.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+   
+    public partial class Gropsserc : Form
+    {
+        public string[] grup = new string[2];
+       public string access_token;
+       public string user_id;
+       string text;
+        public Gropsserc()
+       
+            
+        {
+             
+            InitializeComponent();
+            
+            
+        }
+
+        private void Gropsserc_Load(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            string request = "https://api.vk.com/method/users.get.xml?fields=photo_100&access_token=" + access_token + "&v=5.62";
+            doc.Load(request);
+            if (doc.InnerXml.Contains("error"))
+            {
+                MessageBox.Show("Ошибка получения данных о пользователе!");
+            }
+            foreach (XmlNode level1 in doc.SelectNodes("response"))
+            {
+                foreach (XmlNode level2 in level1.SelectNodes("user"))
+                {
+                    foreach (XmlNode level3 in level2.SelectNodes("first_name"))
+                    {
+                        label1.Text = level3.InnerText;
+                    }
+                    foreach (XmlNode level3 in level2.SelectNodes("last_name"))
+                    {
+                        label2.Text = level3.InnerText;
+                    }
+                    foreach (XmlNode level3 in level2.SelectNodes("photo_100"))
+                    {
+                        pictureBox1.ImageLocation = level3.InnerText;
+                    }
+                }
+            }
+           
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            text = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            XmlDocument serh = new XmlDocument();
+            string ser = "https://api.vk.com/method/groups.search.xml?q=" + text + "&access_token=" + access_token + "&v=5.62";
+            ser = ser.Replace("#", "%23");
+            ser = ser.Replace(" ", "%20");
+            serh.Load(ser);
+            if (serh.InnerXml.Contains("error"))
+            {
+                MessageBox.Show("Ошибка получения данных о пользователе!");
+            }
+            foreach (XmlNode level1 in serh.SelectNodes("response"))
+            {
+                listView1.Items.Clear();
+                foreach (XmlNode level2 in level1.SelectNodes("items"))
+                {
+                    foreach (XmlNode level3 in level2.SelectNodes("group"))
+                    {
+                        string name = "";
+                        string id = "";
+                        
+                        foreach (XmlNode level4 in level3.SelectNodes("name"))
+                        {
+                            grup[0] = level4.InnerText;
+                        }
+                        foreach (XmlNode level4 in level3.SelectNodes("id"))
+                        {
+                            grup[1] = level4.InnerText;
+                        }
+                        foreach (XmlNode level4 in level3.SelectNodes("photo_50"))
+                        {
+                            pictureBox2.Load(level4.InnerText);
+                        }
+                       listView1.Items.Add(id,name,imageList1.Images.Count-1);
+                       imageList1.Images.Add(pictureBox2.Image);
+                        Application.DoEvents();
+                       
+                        ListViewItem grups = new ListViewItem(grup,imageList1.Images.Count - 1);
+                        listView1.Items.Add(grups);
+                    }
+                    
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+           // listView1.SelectedItems[0].SubItems[1].Text; 
+            //listView1.SelectedItems[0].SubItems[0].Text;
+           //listView3 = listView1.SelectedItems[0].SubItems[1].Text;
+          // listView3.Items.Add(listView1.SelectedItems[0].SubItems[0].Text, listView1.SelectedItems[0].SubItems[1].Text); //listView1.SelectedItems[0].SubItems[0].Text;
+            
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView3.Items.Add(listView1.SelectedItems[0].SubItems[0].Text, listView1.SelectedItems[0].SubItems[0].Text); //listView1.SelectedItems[0].SubItems[0].Text;
+                XmlDocument ser = new XmlDocument();
+                string groupsmembers = "https://api.vk.com/method/groups.getMembers.xml?user_id=" + user_id + "&access_token=" + "&fields=photo_100,first_name,last_name" + access_token + "&group_id=" + grup[1] + "&v=5.62"; 
+//foreach (XmlNode ud in ser.SelectNodes("response")) 
+                ser.Load(groupsmembers);
+                //listView1.SelectedItems.Count = listView1.SelectedItems.Count + 1;
+               
+
+                foreach (XmlNode level1 in ser.SelectNodes("response"))
+                {
+                    //listView2.Items.Clear();
+                    foreach (XmlNode level2 in level1.SelectNodes("items"))
+                    {
+                        string name = "";
+                        string id = "";
+                        foreach (XmlNode level3 in level2.SelectNodes("user"))
+                        {
+                        foreach (XmlNode level4 in level3.SelectNodes("id"))
+                        {
+                            id = level4.InnerText;
+                           
+                            // listView1.Items.Add(id,name,imageList1.Images.Count-1);
+                            //imageList1.Images.Add(pictureBox2.Image);
+                           //Application.DoEvents();
+                        }
+                        foreach(XmlNode level4 in level3.SelectNodes("first_name"))
+                        {
+                            name = level4.InnerText;
+                        }
+                        foreach (XmlNode level4 in level3.SelectNodes("last_name"))
+                        {
+                            name = name + level4.InnerText;
+                        }
+                        foreach (XmlNode level4 in level3.SelectNodes("photo_100"))
+                        {
+                            pictureBox2.Load(level4.InnerText);
+                        }
+
+                        ListViewItem itemY = new ListViewItem(name,imageList2.Images.Count - 1);
+                        itemY.SubItems[0] = new ListViewItem.ListViewSubItem(itemY,id);
+                        listView2.Items.Add(itemY);
+                        
+                        imageList2.Images.Add(pictureBox2.Image);
+                        Application.DoEvents();
+                       // ListViewItem grups = new ListViewItem(name,id
+                            
+                        
+                           // Application.DoEvents();
+                            
+                            //listView2.Items.Add(grups);
+                             
+                            foreach (ListViewItem item in listView2.Items)
+                            {
+                                string filters1;
+                                XmlDocument filters = new XmlDocument();
+                                string filter = "https://api.vk.com/method/groups.isMember.xml?group_id=" + grup[1] + "&access_token=" + access_token + "&user_id=" + id + "&v=5.62";
+                              filters.Load(filter);
+                              foreach (XmlNode felter1 in filters.SelectNodes("response"))
+                              {
+                                  filters1 = felter1.InnerText;
+                                  if (filters1 == "0")
+                                  {
+                                      listView2.Items.Remove(itemY);
+                                  }
+                              }
+                            }
+                            
+
+                           
+                        }
+
+                    }
+                }
+                
+                
+            }
+            
+        }    
+    }
+}
